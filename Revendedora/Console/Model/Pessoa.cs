34c5f8fd8236d97852d6da1f74fec64b09ec99a2
Laerte64 <ledurmenegol@gmail.com>
2024-06-01 13:26:07 -0300

    Namespace em Pessoa e Usuario

--- Revendedora/Console/Model/Pessoa.cs
+++ Revendedora/Console/Model/Pessoa.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
-abstract class Pessoa
+namespace Model
 {
-    [Key]
-    public int ID { get; private set; }
-    [Required]
-    [MaxLength(100)]
-    public string Nome { get; private set; }
-    [Required]
-    [MaxLength(11)]
-    public string CPF { get; private set; }
+    abstract class Pessoa
+    {
+        [Key]
+        public int ID { get; private set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; private set; }
+        [Required]
+        [MaxLength(11)]
+        public string CPF { get; private set; }
+    }
 }
--- Revendedora/Console/Model/Usuario.cs
+++ Revendedora/Console/Model/Usuario.cs
@@ -1,26 +1,29 @@
 using System.ComponentModel.DataAnnotations;
 
-class Usuario
+namespace Model
 {
-    [Key]
-    public int ID { get; private set; }
-    [Required]
-    [MaxLength(50)]
-    public string Login { get; private set; }
-    [Required]
-    [MaxLength(100)]
-    private string _senha { get; private set; }
-
-    public bool VerificarSenha(string senha)
+    class Usuario
     {
-        return _senha.Equals(senha);
-    }
+        [Key]
+        public int ID { get; private set; }
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; private set; }
+        [Required]
+        [MaxLength(100)]
+        private string _senha;
 
-    public bool AlterarSenha(string antiga, string nova)
-    {
-        if (!VerificarSenha(antiga))
-            return false;
-        _senha = nova;
-        return true;
+        public bool VerificarSenha(string senha)
+        {
+            return _senha.Equals(senha);
+        }
+
+        public bool AlterarSenha(string antiga, string nova)
+        {
+            if (!VerificarSenha(antiga))
+                return false;
+            _senha = nova;
+            return true;
+        }
     }
 }
