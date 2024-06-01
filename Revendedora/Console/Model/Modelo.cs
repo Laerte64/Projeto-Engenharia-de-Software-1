133420e6e6b0c8488293404133013ffe5b7711cf
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2024-05-31 22:37:17 -0300

    modelo e modelo builder

--- Revendedora/Console/Model/Modelo.cs
+++ Revendedora/Console/Model/Modelo.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Console.Model
 {
-    internal class Modelo
+    public class Modelo
     {
+        public string Nome { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public string Motor { get; set; }
+        public int NumeroPortas { get; set; }
+        public string Transmissao { get; set; }
+        public string Combustivel { get; set; }
+
     }
+
 }
--- Revendedora/Console/Model/ModeloBuilder.cs
+++ Revendedora/Console/Model/ModeloBuilder.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace Console.Model
 {
-    internal class ModeloBuilder
+    class ModeloBuilder
     {
+        private Modelo _modelo;
+
+        public ModeloBuilder()
+        {
+            _modelo = new Modelo();
+        }
+
+        public ModeloBuilder AdicionarNome(string nome)
+        {
+            _modelo.Nome = nome;
+            return this;
+        }
+
+        public ModeloBuilder AdicionarMarca(string marca)
+        {
+            _modelo.Marca = marca;
+            return this;
+        }
+
+        public ModeloBuilder AdicionarAno(int ano)
+        {
+            _modelo.Ano = ano;
+            return this;
+        }
+
+        public ModeloBuilder AdicionarMotor(string motor)
+        {
+            _modelo.Motor = motor;
+            return this;
+        }
+
+        public ModeloBuilder AdicionarNumeroPortas(int num)
+        {
+            _modelo.NumeroPortas = num;
+            return this;
+        }
+
+        public ModeloBuilder AdicionarTransmissao(string transmissao)
+        {
+            _modelo.Transmissao = transmissao;
+            return this;
+        }
+
+        public ModeloBuilder AdicionarCombustivel(string combustivel)
+        {
+            _modelo.Combustivel = combustivel;
+            return this;
+        }
+
+        public Modelo Build()
+        {
+            return _modelo;
+        }
     }
 }
